4eab82c8e03848e53d35e85b14bbec321838c42f
chengweitsai <b01502058@ntu.edu.tw>
2017-06-13 19:32:52 +0800

    choose only SUSY dataset

--- run.sh
+++ run.sh
@@ -13,17 +13,17 @@ a=a9a
 
 i=1
 
-for ratio in "0.1" "0.2" "0.3" "0.4" "0.5" "0.6" "0.7" "0.8" "0.9"
+for ratio in "0.1" "0.2" "0.3" "0.4" "0.5" "0.6" "0.7"  "0.8" "0.9"
 do
 
-for file in  "$SUSY" "$s" "$p" "$rcv1" "$a" "$ij" "$c" "$e" "R"
+for file in  "$SUSY" #"$s" "$p" "$rcv1" "$a" "$ij" "$c" "$e" "R"
 do
-    if [ ! -e output/${file}_ratio_${ratio}.txt ]; then 
-        python dataset_auc.py --dataset $file --request_ratio ${ratio} --output_file ${file}_ratio_${ratio}.txt --batch_size 32 --num_epochs $max --num_time_steps 50000
-    fi
+#    if [ ! -e output/${file}_ratio_${ratio}.txt ]; then 
+#        python dataset_auc.py --dataset $file --request_ratio ${ratio} --output_file ${file}_ratio_${ratio}.txt --batch_size 32 --num_epochs $max --num_time_steps 50000
+#    fi
 
     if [ ! -e output/trad_${file}_ratio_${ratio}.txt ]; then
-        python dataset_acc.py --dataset $file --request_ratio ${ratio} --output_file trad_${file}_ratio_${ratio}.txt --batch_size 32 --num_epoch $max --num_time_steps 50000
+        python2.7 dataset_acc.py --dataset $file --request_ratio ${ratio} --output_file trad_${file}_ratio_${ratio}.txt --batch_size 32 --num_epoch $max --num_time_steps 30000
     fi
 
 done
