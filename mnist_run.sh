ca88cfe8ffd070089a9bf52ade68e51c1984224a
YiTeHong <ted0504@iis.sinica.edu.tw>
2017-06-23 09:00:45 +0800

    add m2_feat_AUC.py

--- mnist_run.sh
+++ mnist_run.sh
@@ -4,13 +4,13 @@ M=mnist
 
 i=1
 
-for ratio in "0.1" "0.2" "0.3" "0.4" "0.5" "0.6" "0.7" "0.8" "0.9" "0.95" "0.99" "0.995"
+for ratio in "0.9" "0.8" "0.3" "0.4" "0.5" "0.6" "0.7" "0.2" "0.1"
 do
 
 for file in "$M"
 do
-    if [ ! -e output/${file}_ratio_${ratio}.txt ]; then 
-        python2.7 new_feat_AUC.py  --request_ratio ${ratio} --output_file ${file}_ratio_${ratio}.txt --batch_size 32 --num_epochs $max --num_time_steps 20000
+    if [ ! -e new_m2_output/${file}_ratio_${ratio}.txt ]; then 
+        python m2_feat_AUC.py  --request_ratio ${ratio} --output_file new_m2_${file}_ratio_${ratio}.txt --batch_size 32  --num_time_steps 20000 --num_epochs 3
     fi
 
     echo $file
